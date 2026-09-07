0b0b25f3388bb50d6e17ed05cac29b6087e393c5
Dominika <dominika@atomy-family.cz>
2026-09-07 12:02:13 +0200

    Karta 3: Svoboda bez zavazku, ikona zamek

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -118,7 +118,7 @@ export default async function HomePage({ params }: Props) {
                   )}
                   {i === 3 && (
                     <svg className="h-6 w-6" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor">
-                      <path strokeLinecap="round" strokeLinejoin="round" d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12z" />
+                      <path strokeLinecap="round" strokeLinejoin="round" d="M13.5 10.5V6.75a4.5 4.5 0 119 0v3.75M3.75 21.75h10.5a2.25 2.25 0 002.25-2.25v-6.75a2.25 2.25 0 00-2.25-2.25H3.75a2.25 2.25 0 00-2.25 2.25v6.75a2.25 2.25 0 002.25 2.25z" />
                     </svg>
                   )}
                 </div>
